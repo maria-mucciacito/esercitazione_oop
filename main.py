b4f8c14568de3d0fe54842512a1a0429f7e7deb1
Maria <96469875+mary023010@users.noreply.github.com>
2022-01-19 10:28:16 +0100

    refactoring1

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 # How to create a class:
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
+    price = None
+    name = ''
+    quantity = None
+
+    def __init__(self,name:str, price:float, quantity=0):
+        #Check price e quantity
+        assert price >= 0, f"Price {price} è minore di 10"
+        assert quantity >= 0, f"La quantità {quantity} è minore di 10"
+        
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 # How to create an instance of a class
-item1 = Item()
-
-# Assign attributes:
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
-
+item1 = Item('Phone',100,5)
 # Calling methods from instances of a class:
-print(item1.calculate_total_price(item1.price, item1.quantity))
+print(item1.calculate_total_price())
 
 # How to create an instance of a class (We could create as much as instances we'd like to)
-item2 = Item()
-
-# Assign attributes
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
-
+item2 = Item("Laptop",1000,3)
 # Calling methods from instances of a class: 
-print(item2.calculate_total_price(item2.price, item2.quantity))
+print(item2.calculate_total_price())
