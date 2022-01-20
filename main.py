ec2380ecdab69a75659c3b57174eb1eb7eaceec4
Maria <96469875+mary023010@users.noreply.github.com>
2022-01-20 12:07:23 +0100

    Iterazione_2

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-# How to create a class:
-class Item:
-    price = None
-    name = ''
-    quantity = None
 
-    def __init__(self,name:str, price:float, quantity=0):
-        #Check price e quantity
-        assert price >= 0, f"Price {price} è minore di 10"
-        assert quantity >= 0, f"La quantità {quantity} è minore di 10"
-        
+class Item:
+    pay_rate = 0.8  #20% sconto
+    all_items = []
+    
+    def __init__(self, name: str, price: float, quantity=0):
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+    #Nota, in alternativa, si possono usare le exceptions
+    #if price < 0:
+    #   raise ValueError(f"Price {price} is not greater than or qaul to zero!")
+    #if quantity < 0:
+    #   raise ValueError(f"Quantity {quantity} is not greater than or equal to zero!")
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        Item.all_items.append(self)
     
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
     def calculate_total_price(self):
-        return self.price * self.quantity
+        return self.apply_discount(self.price) * self.quantity
 
-# How to create an instance of a class
-item1 = Item('Phone',100,5)
-# Calling methods from instances of a class:
-print(item1.calculate_total_price())
+    def apply_discount(self, price):
+        return price * self.pay_rate
 
-# How to create an instance of a class (We could create as much as instances we'd like to)
-item2 = Item("Laptop",1000,3)
-# Calling methods from instances of a class: 
-print(item2.calculate_total_price())
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+print(Item.all_items)
+for i in Item.all_items: 
+    print(i.name + " - " + str(i.calculate_total_price()))
